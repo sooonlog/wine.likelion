330b8e5a4c2025017a704a84f89fa02155dd7c89
sooonlog <93389773+sooonlog@users.noreply.github.com>
2021-12-20 00:05:21 +0900

    ✨ wines 목록페이지 생성

---
+++ src/pages/wines/wines.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+
+const WinesPage: NextPage = () => {
+    return (
+        <div>
+            <h1>
+                WinesPage
+            </h1>
+            <strong>reds</strong>
+            <strong>whites</strong>
+            <strong>sparkling</strong>
+            <strong>rose</strong>
+            <strong>dessert</strong>
+            <strong>port</strong>
+        </div>
+    )
+}
+
+export default WinesPage;
