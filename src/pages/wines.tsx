c681377a3a50ac1b88d0dc389fd6c647815ff6b5
sooonlog <93389773+sooonlog@users.noreply.github.com>
2021-12-20 00:10:00 +0900

    ♻️ wines 목록페이지 코드 및 위치 수정

---
+++ src/pages/wines.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+
+const WinesPage: NextPage = () => {
+    return (
+        <div>
+            <h1>
+                WinesPage
+            </h1>
+            <strong>reds</strong><br />
+            <strong>whites</strong><br />
+            <strong>sparkling</strong><br />
+            <strong>rose</strong><br />
+            <strong>dessert</strong><br />
+            <strong>port</strong><br />
+        </div>
+    )
+}
+
+export default WinesPage;
