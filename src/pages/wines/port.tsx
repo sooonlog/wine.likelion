45f4e1af5884821286a5e18e81316286566be696
sooonlog <gytns0100@gmail.com>
2021-12-23 19:39:20 +0900

    ♻️ wineCardList 추상화

--- src/pages/wines/port.tsx
+++ src/pages/wines/port.tsx
@@ -1,28 +1,12 @@
 import type { NextPage } from "next";
-import { Error, Loading, WineCard } from "../../components";
-import { useWineData } from "../../hooks/useWineData";
-import { Wine } from "../../types/Wine";
+import { WineCardList } from "../../components/WineCardList";
 
 const PortWinePage: NextPage = () => {
     const name = 'port';
-    const { data, error } = useWineData(name);
-
-    if(error) return <Error />
-    if(!data) return <Loading />
 
     return (
         <div>
-            <h1>Port Wine</h1>
-            <main>
-                {data.map((wineData: Wine) => {
-                    return (
-                        <WineCard
-                            key={`port-wine-list-${wineData.id}`}
-                            wineData={wineData}
-                        />
-                    )
-                })}
-            </main>
+           <WineCardList name={name}/>
         </div>
     )
 }
